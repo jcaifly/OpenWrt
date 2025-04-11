8da27f5d83ea2bd033f86c1ade25010dd6133735
jcaifly <38722118+jcaifly@users.noreply.github.com>
2025-04-11 21:56:23 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -18,6 +18,4 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 #echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 #echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-mkdir -p files/etc/opkg
-echo "src/gz openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/mipsel_24kc/kiddin9" >> files/etc/opkg/customfeeds.conf
-echo "option check_signature 0" >> files/etc/opkg.conf
+echo "src-gz openwrt_kiddin9 https://dl.openwrt.ai/latest/packages/mipsel_24kc/kiddin9" >> feeds.conf.default
