42c92e1e8a3387fb44414cc7bbcf295ad065c5fb
jcaifly <38722118+jcaifly@users.noreply.github.com>
2025-04-18 22:41:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -7,28 +7,19 @@
 # --depth 1: 只克隆最近一次 commit，减少下载时间和空间占用
 
 # 1. 添加 OpenClash 软件包及其 LuCI 界面
-# echo '添加 OpenClash...'
-# git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
-# 如果你想用 kiddin9 的版本，并且确定它是一个完整的包目录结构
-mkdir -p package/luci-app-openclash
-svn export https://github.com/kiddin9/kwrt-packages/trunk/luci-app-openclash package/luci-app-openclash
+echo '添加 OpenClash...'
+git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
 
 # 2. 添加 Turbo ACC 网络加速引擎软件包及其 LuCI 界面
-# echo '添加 Turbo ACC...'
-# git clone --depth 1 https://github.com/chenmozhijin/turboacc.git package/luci-app-turboacc
-如果坚持使用 kiddin9 的特定版本
-mkdir -p package/luci-app-turboacc
-svn export https://github.com/kiddin9/kwrt-packages/trunk/luci-app-turboacc package/luci-app-turboacc
+echo '添加 Turbo ACC...'
+git clone --depth 1 https://github.com/chenmozhijin/turboacc.git package/luci-app-turboacc
 
 # 3. 添加关机功能 LuCI 界面
-# echo '添加 PoweroffDevice...'
-# git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
-如果坚持使用 kiddin9 的特定版本
-mkdir -p package/luci-app-poweroffdevice
-svn export https://github.com/kiddin9/kwrt-packages/trunk/luci-app-poweroffdevice package/luci-app-poweroffdevice
+echo '添加 PoweroffDevice...'
+git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
 
 # 4. 添加kmod-nft-fullcone
-# echo '添加 kmod-nft-fullcone...'
-# git clone --depth 1 https://github.com/fullcone-nat-nftables/nft-fullcone.git package/kmod-nft-fullcone
+echo '添加 kmod-nft-fullcone...'
+git clone --depth 1 https://github.com/fullcone-nat-nftables/nft-fullcone.git package/kmod-nft-fullcone
 
 echo 'diy-part1.sh 执行完毕.'
