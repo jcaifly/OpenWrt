b4d81aaba9f70e3f78c753d4bf3deb156feb3eee
jcaifly <38722118+jcaifly@users.noreply.github.com>
2025-11-26 21:37:19 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -1,17 +1,28 @@
 #!/bin/bash
 #
-# https://github.com/P3TERX/Actions-OpenWrt
 # File name: diy-part2.sh
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
-# Copyright (c) 2019-2024 P3TERX <https://p3terx.com>
-#
-# This is free software, licensed under the MIT License.
-# See /LICENSE for more information.
-#
 
-# 1. 修改默认 IP 为 192.168.123.1
+# 1. 设置管理地址为 192.168.123.1
 sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
-# 2. 修改主机名为 OpenWrt (Lede 源码默认可能是 Lede，强制改为 OpenWrt)
+# 2. 设置主机名为 OpenWrt
 sed -i 's/hostname=".*"/hostname="OpenWrt"/g' package/base-files/files/bin/config_generate
+
+# 3. 下载插件源码
+# ----------------------------------------------------------
+
+# (A) 下载 OpenClash
+# 我们先清理可能存在的旧目录，确保干净
+rm -rf package/luci-app-openclash
+git clone --depth 1 https://github.com/vernesong/OpenClash.git package/luci-app-openclash
+
+# (B) 下载 luci-app-poweroffdevice
+rm -rf package/luci-app-poweroffdevice
+git clone --depth 1 https://github.com/sirpdboy/luci-app-poweroffdevice.git package/luci-app-poweroffdevice
+
+# 4. 修复/调整 (针对 Lede 源码的优化)
+# ----------------------------------------------------------
+# 确保 luci-app-poweroffdevice 编译时能找到正确的路径
+# 如果插件结构复杂，通常直接 clone 到 package 根目录即可被识别
