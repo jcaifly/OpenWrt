a3641846c1f499c57541025422d50f6489772d4c
jcaifly <38722118+jcaifly@users.noreply.github.com>
2025-11-26 21:32:37 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,27 +10,8 @@
 # See /LICENSE for more information.
 #
 
-# Modify default IP using uci-defaults (Recommended Method)
-echo "Setting default IP to 192.168.123.1 using uci-defaults..."
-# 创建一个 uci-defaults 脚本来在首次启动时设置 IP
-cat <<EOF > package/base-files/files/etc/uci-defaults/99-default-network-ip
-#!/bin/sh
-uci -q delete network.lan.ipaddr
-uci set network.lan.ipaddr='192.168.123.1'
-uci commit network
-exit 0
-EOF
-echo "Default IP will be set to 192.168.123.1 via uci-defaults on first boot."
+# 1. 修改默认 IP 为 192.168.123.1
+sed -i 's/192.168.1.1/192.168.123.1/g' package/base-files/files/bin/config_generate
 
-# 修改主机名 (使用 uci-defaults 设置为 "OpenWrt")
-echo "Setting default hostname to 'OpenWrt' using uci-defaults..."
-cat <<EOF > package/base-files/files/etc/uci-defaults/98-default-hostname
-#!/bin/sh
-uci -q delete system.@system[0].hostname
-uci set system.@system[0].hostname='OpenWrt' # **在这里将主机名设置为 OpenWrt**
-uci commit system
-exit 0
-EOF
-echo "Default hostname will be set to 'OpenWrt' via uci-defaults on first boot."
-
-echo "diy-part2.sh finished."
+# 2. 修改主机名为 OpenWrt (Lede 源码默认可能是 Lede，强制改为 OpenWrt)
+sed -i 's/hostname=".*"/hostname="OpenWrt"/g' package/base-files/files/bin/config_generate
