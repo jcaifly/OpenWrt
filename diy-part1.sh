a6afd38cee1a6052fc2fe00ae4c61f9fd006635f
jcaifly <38722118+jcaifly@users.noreply.github.com>
2025-11-26 21:40:03 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -4,8 +4,7 @@
 # Description: OpenWrt DIY script part 1 (Before Update feeds)
 #
 
-# 取消注释 helloworld 源 (某些特殊插件可能需要依赖)
+# 取消注释 helloworld 源 (如果你需要 shadowsocksr 等其他协议，可能需要这个)
 # sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
-# 如果你需要添加其他第三方源，可以在这里添加，例如：
-# echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+# 注意：你要求的插件我们在 Part 2 直接下载源码，这里保持干净即可
